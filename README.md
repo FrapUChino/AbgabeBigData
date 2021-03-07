173618685cdb254fde6441e05898d819eb4cd146
Lukas Karsten <l.karsten@ce-con.de>
2021-03-07 20:08:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+Install k3s on OpenStack
+============================
+
+
+Requirements
+------------
+
+-	[Terraform](https://www.terraform.io/downloads.html) 0.12.x
+-	[Ansible]https://www.ansible.com)
+
+
+## Prerequisites
+
+A running Strimzi.io Kafka operator
+
+```bash
+helm repo add strimzi http://strimzi.io/charts/
+helm install my-kafka-operator strimzi/strimzi-kafka-operator
+kubectl apply -f https://farberg.de/talks/big-data/code/helm-kafka-operator/kafka-cluster-def.yaml
+```
+
+A running Hadoop cluster with YARN (for checkpointing)
+
+```bash
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm install --namespace=default --set hdfs.dataNode.replicas=1 --set yarn.nodeManager.replicas=1 --set hdfs.webhdfs.enabled=true my-hadoop-cluster stable/hadoop
+
+Apply the whole thing
+---------------------------
+fürs erste:
+
+- var-Namen wie Passwort, Login, Netzwerk etc. ersetzen
+
+```sh
+$ terraform apply
+$ ansible-playbook -i hosts deploy.yaml
+```
+
+--> deployed k3s cluster, aber nicht die App
+
+set kubectl to remote
+on remote:
+sudo chmod 777 /etc/rancher/k3s/k3s.yaml
+
+scp ubuntu@IP_ADRESS:/etc/rancher/k3s/k3s.yaml .
+"change ip to floatingIp"
+
+```sh
+export KUBECONFIG=/Users/lukas/Documents/Hochschule/S7/BigData/PfisterersAppOnOpenStack/k3s.yaml
+```
---
+++ k3s.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+provider "openstack" {
+        user_name = ""
+        password = ""
+        auth_url = "http://controller.4c.dhbw-mannheim.de:5000/v3"
+        domain_name = "default"
+        tenant_id = "1eee05cb3a2c4a3f9b93d79359e20471 "
+}
+
+resource "openstack_compute_instance_v2" "lukas_k3s_master" {
+  name = "lukas_k3s_master"
+  image_id = "a0a1c616-f4f3-429d-8de9-8e74b5df805c"
+  flavor_name = "m1.medium"
+  security_groups = ["default"]
+  key_pair = "LukasRSA"
+
+  network {
+          name = "lukas_network"
+  }
+}
+
+
+resource "openstack_compute_instance_v2" "lukas_k3s_nodes" {
+  count=3
+  name= format("lukas_k3s_node_%s", count.index)
+  image_id = "a0a1c616-f4f3-429d-8de9-8e74b5df805c"
+  flavor_name = "m1.medium"
+  security_groups = ["default"]
+  key_pair = "LukasRSA"
+
+  network {
+          name = "lukas_network"
+  }
+}
+
+resource "openstack_networking_floatingip_v2" "lukas_floating_node_ip" {
+        count=3
+        pool ="ext-net-201"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "associate_node_ips" {
+        count = length(openstack_compute_instance_v2.lukas_k3s_nodes)
+        floating_ip = "${openstack_networking_floatingip_v2.lukas_floating_node_ip[count.index].address}"
+        instance_id = openstack_compute_instance_v2.lukas_k3s_nodes[count.index].id
+}
+
+resource "openstack_networking_floatingip_v2" "lukas_floating_ip" {
+        pool ="ext-net-201"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "associate" {
+        floating_ip = openstack_networking_floatingip_v2.lukas_floating_ip.address
+        instance_id = openstack_compute_instance_v2.lukas_k3s_master.id
+}
+
+
+
+#Write Floating IP to file
+data "template_file" "ansible_inventory" {
+  template = "${file("${path.module}/hosts.tmpl")}"
+
+  vars = {
+     master_pub_ip = "${openstack_networking_floatingip_v2.lukas_floating_ip.address}"
+     node_pub_ips = "${join("\n",openstack_networking_floatingip_v2.lukas_floating_node_ip.*.address)}"
+  }
+}
+
+resource "local_file" "hosts" {
+  filename = "${path.module}/hosts"
+  content = data.template_file.ansible_inventory.rendered
+}
+
+
+resource "local_file" "floating_id_file" {
+        content = openstack_networking_floatingip_v2.lukas_floating_ip.address
+        filename = "${path.module}/openstack-inventory.txt"
+}
